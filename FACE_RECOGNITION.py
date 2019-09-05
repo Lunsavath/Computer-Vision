ccaed170b81f04f938f496fbbbfe54989b88f556
Lunsavath <anush@nearbuzz.com>
2019-09-05 15:23:53 +0530

    added smile feature

--- FACE_RECOGNITION.py
+++ FACE_RECOGNITION.py
@@ -6,23 +6,27 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 #Defining a function to detect faces
 
 def detect(gray, frame):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 3)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,0), 2)
+        smile = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
+        for (sx, sy, sw, sh) in eyes:
+            cv2.rectangle(roi_color, (sx,sy), (sx+sw, sy+sh), (0, 0, 255), 2)
     return frame
 
 #Detecting the face from the webcam
 
-video_capture = cv2.VideoCapture(-1)
+video_capture = cv2.VideoCapture(2)
 while True:
     ret,frame = video_capture.read()
     if ret == False:
